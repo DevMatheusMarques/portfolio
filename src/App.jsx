e87e14343ca3669d911aa182a1d070932491b17c
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-11-18 07:29:06 -0300

    feat(project): Base Project.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./js/sections/Header.jsx";
+import AboutMe from "./js/sections/AboutMe.jsx";
+import Experience from "./js/sections/Experience.jsx";
+import ProjectsReact from "./js/sections/ProjectsReact.jsx";
+import ProjectsVue from "./js/sections/ProjectsVue.jsx";
+import Footer from "./js/sections/Footer.jsx";
+
+function App() {
+
+  return (
+    <>
+        <Header/>
+        <AboutMe/>
+        <Experience/>
+        <ProjectsReact/>
+        <ProjectsVue/>
+        <Footer/>
+    </>
+  )
+}
+
+export default App
